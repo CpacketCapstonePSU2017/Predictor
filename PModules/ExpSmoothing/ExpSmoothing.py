36c46af2b932850f22c3aaa63441f4b772ebef44
Andrew Gonser <gonsera@gmail.com>
2018-03-01 12:43:07 -0800

    Added comments and formatted the output into an np_array.

--- PModules/ExpSmoothing/ExpSmoothing.py
+++ PModules/ExpSmoothing/ExpSmoothing.py
@@ -9,20 +9,36 @@ import math
 class ExpSmoothing:
 
     def __init__(self, series, alpha, csv_filename):
+        """
+        The parameters for this function are:
+        :param series: The series passed in from call model.
+        :param alpha: The alpha for N.
+        :param csv_filename: The name of the CSV file with the series data.
+        """
         self.default_series = series
         self.default_alpha = alpha
         self.default_csv_filename = os.path.join(RESOURCES_DIR, csv_filename)
 
     def gen_weights(self, alpha, number_of_weights):
+        """
+        The parameters for a function that generates weights:
+        :param alpha: The alpha for N.
+        :param number_of_weights: The amount of weeks observed in the data.
+        :return: A list the size of number_of_weights with weights from alpha that approach 0.
+        """
         ws =list()
         for i in range(number_of_weights):
             w = alpha * ((1-alpha)**i)
             ws.append(w)
         return ws
 
-    #def weighted(self):
-    # given a series and alpha, return series of smoothed points
     def exponential_smoothing(self, series, alpha):
+        """
+        The parameters to make an exponential smoothing prediction
+        :param series: This is the series that is passed in from main that has the data to be processed.
+        :param alpha: The starting weight.
+        :return:
+        """
         result = [series[0]]  # first value is same as series
         length = len(series)
         weeks_to_count = math.floor(length/672)
@@ -51,33 +67,25 @@ class ExpSmoothing:
                 result.append(total)
         return result
 
-    # >>> exponential_smoothing(series, 0.1)
-    # [3, 3.7, 4.53, 5.377, 6.0393, 6.43537, 6.991833]
-    # >>> exponential_smoothing(series, 0.9)
-    # [3, 9.3, 11.73, 12.873000000000001, 12.0873, 10.20873, 11.820873]
-
-
     def call_model(self):
         # build dataframe
-        df = cdf.csv_to_dataframe("/home/pirate/Desktop/AccessPoint1Incoming.csv", 0, 29474, False, [1])
+        df = cdf.csv_to_dataframe(self.default_csv_filename,0,34945 , False, [0,1])
         series = list(df.values.flatten())
-        # FIXME: remove debug code
-        print(series)
-        print(len(series))  # pick desired seasonal length, alpha, beta, gamma and desired number of predicted points
-        self.default_series = series  # call triple_exponential_smoothing with series = byte counts column in dataframe
+        bytcts = series[1::2]
+        self.default_series = bytcts  # call triple_exponential_smoothing with series = byte counts column in dataframe
         smooth_series = self.exponential_smoothing(self.default_series, self.default_alpha)
-        # FIXME: remove debug code
         print(smooth_series)
-        print(len(smooth_series))
+        result_datetimes = pd.date_range(series[-2], periods=674, freq='15min')[1:]
+        nparray_data = np.array([result_datetimes, smooth_series]).transpose()
+        """
+        #print(nparray_data)
+        #print(len(nparray_data))
         #matplotlib.pyplot.plot(smooth_series)
         #matplotlib.pyplot.ylabel('some numbers')
         #matplotlib.pyplot.show()  # pass back series with N new predicted results
-        # generate N new new sequential timestamps (per 15 min)
-        # assign new timestamps to datapoints
-        # append new timestamp:datapoint pairs to original dataframe
-        # pass back completed dataframe or generate new csv file.
-        return 1  # FIXME: Remove this test code at end. Only for testing class
+        """
+        return nparray_data
 
 
-test = ExpSmoothing(None, 0.2, 'temp.csv')
-test.call_model()
+#test = ExpSmoothing(None, 0.2, "/home/pirate/Desktop/AccessPoint1Incoming.csv")
+#test.call_model()
